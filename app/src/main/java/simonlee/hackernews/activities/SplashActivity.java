68b692cb2e25792291436fe3d056ee9bfd5607d3
Leetsong <leetsong.lc@gmail.com>
2018-07-15 13:01:21 +0800

    Initialize the project

---
+++ app/src/main/java/simonlee/hackernews/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package simonlee.hackernews.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import simonlee.hackernews.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
